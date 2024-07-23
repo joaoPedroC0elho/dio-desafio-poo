9cd8f5807e373bb7cf20568e965d2f65b8cef8c1
joaoPedroC0elho <96500747+joaoPedroC0elho@users.noreply.github.com>
2024-07-23 09:33:42 -0300

    Add files via upload

---
+++ Desafio-poo-dio/src/Conteudo.java
@@ -0,0 +1,30 @@
+public class Conteudo {
+
+
+    protected static final  double XP_PRADAO = 10d;
+
+    private  String titulo;
+
+    private  String descricao;
+
+    public double calcularXp() {
+        return 0;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
